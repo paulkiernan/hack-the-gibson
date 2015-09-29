f215cbc7f1b0d2a41f893f537890f0eaf4e20c45
Paul Kiernan <paul@chartbeat.com>
2015-09-29 15:37:17 -0400

    Initial commit for a screensaver project

---
+++ MacBuild/gibson/gibsonView.h
@@ -0,0 +1,13 @@
+//
+//  gibsonView.h
+//  gibson
+//
+//  Created by Paul Kiernan on 9/29/15.
+//  Copyright © 2015 Paul Kiernan. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface gibsonView : ScreenSaverView
+
+@end
---
+++ MacBuild/gibson/gibsonView.m
@@ -0,0 +1,52 @@
+//
+//  gibsonView.m
+//  gibson
+//
+//  Created by Paul Kiernan on 9/29/15.
+//  Copyright © 2015 Paul Kiernan. All rights reserved.
+//
+
+#import "gibsonView.h"
+
+@implementation gibsonView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
