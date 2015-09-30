25c77bd01c27b117ed7e141a2c743e712b9abbb3
Paul Kiernan <paul@chartbeat.com>
2015-09-30 11:11:52 -0400

    Docs

--- MacBuild/gibson/gibsonView.m
+++ MacBuild/gibson/gibsonView.m
@@ -10,40 +10,49 @@
 
 @implementation gibsonView
 
+// initializes the view with the given frame rect
 - (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
 {
     self = [super initWithFrame:frame isPreview:isPreview];
     if (self) {
+        // sets the animation rate in seconds
         [self setAnimationTimeInterval:1/30.0];
     }
     return self;
 }
 
+// invoked by the screen saver engine when animation should begin
 - (void)startAnimation
 {
     [super startAnimation];
 }
 
+// invoked by the screen saver engine when animation should stop
 - (void)stopAnimation
 {
     [super stopAnimation];
 }
 
+// draws the view
 - (void)drawRect:(NSRect)rect
 {
     [super drawRect:rect];
 }
 
+// advances the animation by one frame at the rate set by the
+// preceding method (this can be used for drawing instead of drawRect:)
 - (void)animateOneFrame
 {
     return;
 }
 
+// returns true if the module has a configure sheet
 - (BOOL)hasConfigureSheet
 {
     return NO;
 }
 
+// returns the module's associated configure sheet
 - (NSWindow*)configureSheet
 {
     return nil;
